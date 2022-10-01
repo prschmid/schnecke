44cbdfaa51278fc75434dac1d94e57ebe1ee6885
Patrick R. Schmid <prschmid@gmail.com>
2022-10-01 16:56:37 -0400

    Set maximum slug length

--- test/schnecke_test.rb
+++ test/schnecke_test.rb
@@ -77,6 +77,52 @@ class SchneckeTest < Minitest::Test
     assert_equal 'duplicate-name-2', obj3.slug
   end
 
+  def test_slug_is_truncated_to_32_characters_by_default
+    create_dummy_schnecke_models_table
+
+    obj = DummySchneckeModel.new(
+      name: 'This name has more than 32 characters and will be truncated'
+    )
+    assert_nil obj.slug
+
+    obj.assign_slug
+
+    assert_equal 32, obj.slug.length
+    assert_equal 'this-name-has-more-than-32-chara', obj.slug
+  end
+
+  def test_uniq_sequence_id_is_added_after_truncating_slug
+    create_dummy_schnecke_models_table
+
+    obj1 = DummySchneckeModel.create(
+      name: 'This name has more than 32 characters and will be truncated'
+    )
+    assert_equal 32, obj1.slug.length
+    assert_equal 'this-name-has-more-than-32-chara', obj1.slug
+
+    obj2 = DummySchneckeModel.create(
+      name: 'This name has more than 32 characters and will be truncated'
+    )
+    assert_equal 34, obj2.slug.length
+    assert_equal 'this-name-has-more-than-32-chara-2', obj2.slug
+  end
+
+  def test_can_change_maximum_length_of_slug
+    create_dummy_schnecke_short_slug_models_table
+
+    # The dummy table has the max length set to 15 characters unlike the
+    # default of 32
+    obj = DummySchneckeShortSlugModel.new(
+      name: 'This name has more than 15 characters and will be truncated'
+    )
+    assert_nil obj.slug
+
+    obj.assign_slug
+
+    assert_equal 15, obj.slug.length
+    assert_equal 'this-name-has-m', obj.slug
+  end
+
   def test_can_assign_slug_to_non_default_column
     create_dummy_schnecke_non_default_column_models_table
 
@@ -134,6 +180,44 @@ class SchneckeTest < Minitest::Test
     assert_equal 'child-of-parent-2', child22.slug
   end
 
+  def test_can_set_custom_uniqueness_scope_for_polymorphic_relationships
+    create_dummy_schnecke_uniquness_scope_polymorphic_models_table
+
+    parent1 = DummySchneckePolymorphicAssociationModel.create!(name: 'Parent 1')
+    child11 = DummySchneckeUniqueScopePolymorphicModel.create!(
+      dummy_schnecke_association_model_type:
+        'DummySchneckePolymorphicAssociationModel',
+      dummy_schnecke_association_model_id: parent1.id,
+      name: 'child of parent'
+    )
+    assert_equal 'child-of-parent', child11.slug
+
+    child12 = DummySchneckeUniqueScopePolymorphicModel.create!(
+      dummy_schnecke_association_model_type:
+        'DummySchneckePolymorphicAssociationModel',
+      dummy_schnecke_association_model_id: parent1.id,
+      name: 'child of parent'
+    )
+    assert_equal 'child-of-parent-2', child12.slug
+
+    parent2 = DummySchneckePolymorphicAssociationModel.create!(name: 'Parent 2')
+    child21 = DummySchneckeUniqueScopePolymorphicModel.create!(
+      dummy_schnecke_association_model_type:
+        'DummySchneckePolymorphicAssociationModel',
+      dummy_schnecke_association_model_id: parent2.id,
+      name: 'child of parent'
+    )
+    assert_equal 'child-of-parent', child21.slug
+
+    child22 = DummySchneckeUniqueScopePolymorphicModel.create!(
+      dummy_schnecke_association_model_type:
+        'DummySchneckePolymorphicAssociationModel',
+      dummy_schnecke_association_model_id: parent2.id,
+      name: 'child of parent'
+    )
+    assert_equal 'child-of-parent-2', child22.slug
+  end
+
   private
 
   def create_dummy_schnecke_models_table
@@ -144,8 +228,18 @@ class SchneckeTest < Minitest::Test
       with_columns do |t|
         t.string :name, null: false, default: 'name'
         t.string :slug
+      end
+    end
+  end
 
-        t.timestamps index: true
+  def create_dummy_schnecke_short_slug_models_table
+    Temping.create :dummy_schnecke_short_slug_models do
+      include Schnecke
+      slug :name, limit_length: 15
+
+      with_columns do |t|
+        t.string :name, null: false, default: 'name'
+        t.string :slug
       end
     end
   end
@@ -159,8 +253,6 @@ class SchneckeTest < Minitest::Test
         t.string :name, null: false, default: 'name'
         t.string :slug
         t.string :another_slug_column
-
-        t.timestamps index: true
       end
     end
   end
@@ -174,8 +266,6 @@ class SchneckeTest < Minitest::Test
         t.string :first_name, null: false, default: 'first'
         t.string :last_name, null: false, default: 'last'
         t.string :slug
-
-        t.timestamps index: true
       end
     end
   end
@@ -185,8 +275,6 @@ class SchneckeTest < Minitest::Test
       with_columns do |t|
         t.string :name, null: false, default: 'first'
         t.string :slug
-
-        t.timestamps index: true
       end
     end
 
@@ -200,8 +288,35 @@ class SchneckeTest < Minitest::Test
         t.bigint :dummy_schnecke_association_model_id, null: false
         t.string :name, null: false, default: 'first'
         t.string :slug
+      end
+    end
+  end
 
-        t.timestamps index: true
+  def create_dummy_schnecke_uniquness_scope_polymorphic_models_table
+    Temping.create :dummy_schnecke_polymorphic_association_models do
+      with_columns do |t|
+        t.string :name, null: false, default: 'first'
+        t.string :slug
+      end
+    end
+
+    Temping.create :dummy_schnecke_unique_scope_polymorphic_models do
+      include Schnecke
+      slug :name,
+           uniqueness: {
+             scope: %i[
+               dummy_schnecke_association_model_type
+               dummy_schnecke_association_model_id
+             ]
+           }
+
+      belongs_to :dummy_schnecke_association_model, polymorphic: true
+
+      with_columns do |t|
+        t.string :dummy_schnecke_association_model_type, null: false
+        t.bigint :dummy_schnecke_association_model_id, null: false
+        t.string :name, null: false, default: 'first'
+        t.string :slug
       end
     end
   end
